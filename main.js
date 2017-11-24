cc02fdb31278787c3391dfedeed65cf31cf3b089
ppasler <ppasler@seibert-media.net>
2017-11-24 20:55:59 +0100

    added underscore and range

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 		e.preventDefault();
 		console.log("calculate");
 
-		apfel($("#re_min").val(), $("#im_min").val(), $("#re_max").val(), $("#im_max").val(), 2, 1000, 1000, 100);
+		var xPixels = _.range(1, 100);
+		var yPixels = _.range(1, 100);
+		apfel($("#re_min").val(), $("#im_min").val(), $("#re_max").val(), $("#im_max").val(), 2, xPixels, yPixels, 100);
 	});
 });
