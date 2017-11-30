2dc33bd6c4fab03bde282ced82f48db690829ddb
ppasler <ppasler@seibert-media.net>
2017-11-30 20:39:21 +0100

    added simple zoom and fixed imaginary axes

--- src/js/apfelmaennchen-init.js
+++ src/js/apfelmaennchen-init.js
@@ -3,36 +3,55 @@
 
 	var WIDTH = 900;
 	var HEIGHT = 600;
+	var ZOOM_FACTOR = 0.75;
+
+	var zoom = 1;
+
+	var canvas;
+	var calculator;
+	var renderer;
 
 	function init() {
-		$("#calculate").click(initClickHandler);
+		createCanvas(WIDTH, HEIGHT);
+		calculator = new Calculator(WIDTH, HEIGHT);
+		renderer = new Renderer(WIDTH, HEIGHT);
+
+		$("#calculate").on("click", function (e) {
+			e.preventDefault();
+			calcApfelmaennchen();
+		});
+		$("#canvas").on("click", function (e) {
+			zoom *= ZOOM_FACTOR;
+			calcApfelmaennchen();
+			getCursorPosition(e);
+		});
 	}
 
-	function initClickHandler(e) {
-		e.preventDefault();
-
-		var canvas = createCanvas(WIDTH, HEIGHT);
-		var calculator = new Calculator(WIDTH, HEIGHT);
-		var renderer = new Renderer(WIDTH, HEIGHT);
-
+	function calcApfelmaennchen() {
 		var realMin = getValue("#re_min");
 		var realMax = getValue("#re_max");
-		var imaMin = getValue("#im_min");
-		var imaMax = getValue("#im_max");
+		var imaMin = getValue("#im_min") * -1; // TODO axes switched
+		var imaMax = getValue("#im_max") * -1;
 		var maxAbsolute = getValue("#maxAbsolute");
 		var maxIterations = getValue("#maxIterations");
 
-		var image = calculator.calc(realMin, realMax, imaMin, imaMax, maxAbsolute, maxIterations);
+		var image = calculator.calc(realMin * zoom, realMax * zoom, imaMax * zoom, imaMin * zoom, maxAbsolute, maxIterations);
 		renderer.render(canvas, image, maxIterations);
 	}
 
+	function getCursorPosition(event) {
+		var rect = canvas.getBoundingClientRect();
+		var x = event.clientX - rect.left;
+		var y = event.clientY - rect.top;
+		return {x: x, y: y};
+	}
+
 	function createCanvas(width, height) {
-		var canvas = document.createElement('canvas');
+		canvas = document.createElement('canvas');
 		canvas.width = width;
 		canvas.height = height;
 		$("#canvas").html(canvas);
 		console.log("created canvas", canvas);
-		return canvas;
 	}
 
 	function getValue(id) {
